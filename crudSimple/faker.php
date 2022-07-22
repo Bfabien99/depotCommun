c33ec14a53c4a2f214585bcf7046a1a3d1a57511
Bfabien99 <fabienbrou99@gmail.com>
2022-07-22 11:58:41 +0000

    faker to fill database

--- crudSimple/faker.php
+++ crudSimple/faker.php
@@ -1,2 +1,20 @@
 <?php
 require_once '../vendor/autoload.php';
+require_once('config.php');
+
+$faker = Faker\Factory::create('fr_FR');
+
+
+for ($i=0; $i < 3; $i++) { 
+    $query = $connect->prepare("INSERT INTO `articles` (`titre`, `description`, `url_image`,`date_publication`) VALUES (:titre,:description,:url_image,:date)");
+    $ajouter = $query->execute([
+        "titre" => $faker->realText(mt_rand(30,45)),
+        "description" => $faker->realText(200,3) . "<br>" . $faker->realText(200,2),
+        "url_image" => $faker->imageUrl(),
+        "date" => $faker->dateTimeBetween('-' . mt_rand(1,3) . 'month')->format('Y-m-d H:i:s')
+    ]);
+
+    if($ajouter){
+        echo '<div class="success">Nouvelle donnée enregistré!</div>';
+    }
+}
